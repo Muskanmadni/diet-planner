0dc99015323e3c51e690d3ed8d1a55fbe238c572
umm e habiba madni <88267142+Muskanmadni@users.noreply.github.com>
2025-11-15 11:13:20 +0500

    first commit

--- src/diet_planner/app.py
+++ src/diet_planner/app.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_cors import CORS
 import os
+
 import google.generativeai as genai
 from dotenv import load_dotenv
 from datetime import datetime, timedelta
