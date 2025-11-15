3e3c8ffb99d7ae3574be497c4aa4a732bde1f96a
umm e habiba madni <88267142+Muskanmadni@users.noreply.github.com>
2025-11-15 11:03:29 +0500

    first commit

--- wsgi.py
+++ wsgi.py
@@ -1,12 +1,12 @@
 import sys
 import sys
 import os
+from src.diet_planner.app import app
 
 # Add the project root to the Python path to ensure modules can be found
 project_root = os.path.dirname(os.path.abspath(__file__))
 sys.path.insert(0, project_root)
 
-from src.diet_planner.app import app
 
 # For Vercel deployment, the WSGI server will look for 'application' as the WSGI callable
 application = app
