0ac641328d7230cbad15c0bf06f42e606e34f79d
umm e habiba madni <88267142+Muskanmadni@users.noreply.github.com>
2025-11-20 02:13:51 +0500

    first commit

--- src/diet_planner/app.py
+++ src/diet_planner/app.py
@@ -67,8 +67,6 @@ def login_required(f):
 # User model
 
 class User(db.Model):
-    __tablename__ = 'users'  # Yeh line missing thi – isliye error aa raha tha!
-
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password_hash = db.Column(db.String(128), nullable=False)
